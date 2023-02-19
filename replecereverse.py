6e9bd044dd108a76e796d30301699c86adbfdd0a
Awa Danfa <31821970+AwaDanfa@users.noreply.github.com>
2023-02-19 12:20:07 +0000

    Added initial file

---
+++ replecereverse.py
@@ -0,0 +1,9 @@
+""" • Save the sentence: “I!am!a!coding!genius!” as a single string.
+• Reprint this sentence as “I am a coding genius” using the replace() function to change every exclamation mark(!) to a blank space.
+• Print the sentence in reverse.
+ """
+
+sentence = "I!am!a!coding!genius!"
+sentence = sentence.replace("!", " ")
+print(sentence)
+print(sentence[::-1])
